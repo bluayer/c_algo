59540ad4c3e597862427577dbd3218ba9bfd1289
bluayer <ijacsong98@gmail.com>
2018-04-06 16:34:40 +0900

    2747 in java

---
+++ 2747.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int input = sc.nextInt();
+		System.out.print(fibo(input));
+	}
+	
+	public static int fibo(int n) {
+		if (n <= 1 ) {
+			return n;
+		}
+		else {
+			return fibo(n-2) + fibo(n-1); 
+		}
+	}
+
+}
